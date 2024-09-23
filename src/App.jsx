16f366950bb8df185c677aaaad9d263954e4b0e6
JulesBestDev176 <souleymanefall176@gmail.com>
2024-09-23 00:10:34 +0000

    frontend right

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,13 @@
 import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import Container from './components/Container'
-import Accueil from './pages/Accueil'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Produits from './pages/Produits'
 
 function App() {
 
-
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Container />}>
-            <Route index element={<Accueil />} />
-            <Route path="about" element={<About />} />
-            <Route path="contact" element={<Contact />} />
-            <Route path="/produits" element={<Produits />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+
     </>
   )
 }
